74cbe53cc36d9905fa90e91ed18ac81ffaae6f64
Gourab Sen <gourabsen@Gourabs-MacBook-Air.local>
2025-01-19 14:52:57 +0530

    REPL1

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,9 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  std::cout << "$ ";
   
   for(;;){
+  std::cout << "$ ";
   std::string input;
   std::getline(std::cin, input);
   if(input == "exit 0") return 0;
