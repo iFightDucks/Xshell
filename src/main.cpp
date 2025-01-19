eb5d410fa255df1bc36c66a4f9dd897bab3c7365
Gourab Sen <gourabsen@Gourabs-MacBook-Air.local>
2025-01-19 15:23:55 +0530

    type builtin

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,14 @@ int main() {
   std::cout<<"$ ";
   std::string input;
   while(std::getline(std::cin, input) && input.find("exit") != 0){
+    if(input.substr(0,5) == "type"){
+      std::string cmd = input.substr(5);
+      if(cmd.substr(0,4) == "type" || cmd.substr(0,4) == "echo" || cmd.substr(0,4) == "exit"){
+        std::cout<< cmd << " is a shell builtin\n";
+      }else{
+        std::cout<<cmd<<" not found\n";
+      }
+    }
     if(input.find("echo") == 0){
       const int ECHO_LEN = 5;
       std::string text = input.substr(ECHO_LEN);
