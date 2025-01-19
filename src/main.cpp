f0fba424a465431abb9992d8fb25822ad4f2b9af
Gourab Sen <gourabsen@Gourabs-MacBook-Air.local>
2025-01-19 16:15:36 +0530

    Path

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include <string>
 #include <sstream>
 #include <filesystem>
+
 using namespace std;
 
-enum validCommands
-{
+enum validCommands {
     echo,
     cd,
     exit0,
@@ -13,65 +13,80 @@ enum validCommands
     invalid,
 };
 
-validCommands isValid(std::string command){
-    command = command.substr(0,command.find(" "));
-    if(command == "echo") return validCommands::echo;
-    if(command == "cd") return validCommands::cd;
-    if(command == "exit") return validCommands::exit0;
-    if(command == "type") return validCommands::type;
+validCommands isValid(std::string command) {
+    command = command.substr(0, command.find(" "));
+    if (command == "echo") return validCommands::echo;
+    if (command == "cd") return validCommands::cd;
+    if (command == "exit") return validCommands::exit0;
+    if (command == "type") return validCommands::type;
     return invalid;
 }
 
+
 std::string valid[4] = {"echo", "cd", "exit0"};
 
-std::string get_path(std::string command){
+
+std::string get_path(std::string command) {
     std::string path_env = std::getenv("PATH");
     std::stringstream ss(path_env);
     std::string path;
-    while(!ss.eof()){
+
+    while (!ss.eof()) {
         getline(ss, path, ':');
         string abs_path = path + '/' + command;
-        if(filesystem::exists(abs_path)){
+        if (filesystem::exists(abs_path)) {
             return abs_path;
         }
     }
-    return "";  
+    return "";
 }
 
 int main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::string input;
-    while(true){
-        std::cout<<"$ ";
+
+    bool exit = false;
+
+    while (!exit) {
+      
+        std::cout << std::unitbuf;
+        std::cerr << std::unitbuf;
+
+        std::cout << "$ ";
+        std::string input;
         std::getline(std::cin, input);
-        if(input.empty()) continue;
-        validCommands cmd = isValid(input);
-        switch(cmd){
-            case echo:
-                std::cout<<input.substr(5)<<std::endl;
-                break;
+
+        switch (isValid(input)) {
             case cd:
-                std::cout<<"cd command is not implemented\n";
+  
                 break;
+
+            case echo: {
+                input.erase(0, input.find(" ") + 1); 
+                std::cout << input << "\n";
+                break;
+            }
+
             case exit0:
-                return 0;
-            case type:
-                input = input.substr(5);
-                if(input == "echo" || input == "cd" || input == "exit"){
-                    std::cout<<input<<" is a shell builtin\n";
-                }
-                else{
+                exit = true;
+                break;
+
+            case type: {
+                input.erase(0, input.find(" ") + 1);
+                if (isValid(input) != invalid) {
+                    std::cout << input << " is a shell builtin\n";
+                } else {
                     std::string path = get_path(input);
-                    if(path.empty()){
-                        std::cout<<input<<" not found\n";
-                    }
-                    else{
-                        std::cout<<input<<" is "<<path<<std::endl;
+                    if (path.empty()) {
+                        std::cout << input << " not found\n";
+                    } else {
+                        std::cout << input << " is " << path << std::endl;
                     }
                 }
                 break;
+            }
+
             default:
-                std::cout<<input<<": command not found\n";
+
+                std::cout << input << ": command not found\n";
                 break;
         }
     }
