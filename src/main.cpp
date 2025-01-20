3e97e300257f2233753e85da2844a59e09818fb1
Gourab Sen <gourabsen@Gourabs-MacBook-Air.local>
2025-01-20 10:45:32 +0530

    Type Error Fixed

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,6 @@ enum validCommands
     type,
     invalid,
 };
-
 validCommands isValid(std::string command) {
     command = command.substr(0, command.find(" "));
     if (command == "echo") return validCommands::echo;
@@ -23,6 +22,22 @@ validCommands isValid(std::string command) {
     if (command == "exit") return validCommands::exit0;
     if (command == "type") return validCommands::type;
     return invalid;
+}
+
+
+std::string get_path(std::string command) {
+    std::string path_env = std::getenv("PATH");
+    std::stringstream ss(path_env);
+    std::string path;
+
+    while (!ss.eof()) {
+        getline(ss, path, ':');
+        std::string abs_path = path + '/' + command;
+        if (filesystem::exists(abs_path)) {
+            return abs_path;
+        }
+    }
+    return "";
 }
 
 void execute_command(const std::string& command, const std::string& args) {
@@ -38,6 +53,7 @@ void execute_command(const std::string& command, const std::string& args) {
     std::string result;
     bool command_output = false;
 
+
     while (fgets(buffer.data(), buffer.size(), fp) != nullptr) {
         result += buffer.data();
         command_output = true;
@@ -45,10 +61,8 @@ void execute_command(const std::string& command, const std::string& args) {
 
     int exit_status = pclose(fp);
     if (exit_status != 0 && !command_output) {
-        
         std::cout << command << ": command not found\n";
     } else {
-        
         std::cout << result;
     }
 }
@@ -73,21 +87,31 @@ int main() {
         switch (isValid(command)) {
             case cd:
                 break;
+
             case echo:
                 input.erase(0, input.find(" ") + 1);
                 std::cout << input << "\n";
                 break;
+
             case exit0:
                 exit = true;
                 break;
-            case type:
+
+            case type: {
                 input.erase(0, input.find(" ") + 1);
                 if (isValid(input) != invalid) {
                     std::cout << input << " is a shell builtin\n";
                 } else {
-                    std::cout << input << ": command not found\n";
+                    std::string path = get_path(input);
+                    if (path.empty()) {
+                        std::cout << input << ": command not found\n";
+                    } else {
+                        std::cout << input << " is " << path << "\n";
+                    }
                 }
                 break;
+            }
+
             default:
                 execute_command(command, args);
                 break;
