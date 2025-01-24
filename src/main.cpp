875ef12bedaf7efa33016169eba089dc4b55993f
Gourab Sen <gourabsen@Gourabs-MacBook-Air.local>
2025-01-25 02:31:51 +0530

    double quotes 2

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,14 @@
 
 using namespace std;
 
-enum validCommands { echo, cd, exit0, type, invalid, pwd };
+enum validCommands {
+    echo,
+    cd,
+    exit0,
+    type,
+    invalid,
+    pwd
+};
 
 validCommands isValid(const std::string& command) {
     std::string cmd = command.substr(0, command.find(" "));
@@ -25,7 +32,6 @@ std::string get_path(const std::string& command) {
     std::string path_env = std::getenv("PATH");
     std::stringstream ss(path_env);
     std::string path;
-
     while (!ss.eof()) {
         getline(ss, path, ':');
         std::string abs_path = path + '/' + command;
@@ -36,58 +42,40 @@ std::string get_path(const std::string& command) {
     return "";
 }
 
-std::string processQuotes(const std::string& input) {
+std::string processEchoInput(const std::string& input) {
     std::string result;
-    bool inSingleQuote = false, inDoubleQuote = false;
     bool escape = false;
-
     for (size_t i = 0; i < input.length(); ++i) {
         char c = input[i];
-
         if (escape) {
-            if (c == ' ') {
-                result += ' ';
-            } else {
+            if (c == 'n') result += '\n';
+            else if (c == '\\') result += '\\';
+            else {
+                result += '\\';
                 result += c;
             }
             escape = false;
-            continue;
-        }
-
-        if (c == '\\') {
-            escape = true;
-        } else if (c == '\'' && !inDoubleQuote) {
-            result += c;
-            inSingleQuote = !inSingleQuote;
-        } else if (c == '\"' && !inSingleQuote) {
-            result += c;
-            inDoubleQuote = !inDoubleQuote;
-        } else {
-            result += c;
-        }
+        } else if (c == '\\') escape = true;
+        else result += c;
     }
-
+    if (escape) result += '\\';
     return result;
 }
 
 void execute_command(const std::string& command, const std::string& args) {
     std::string full_command = command + " " + args + " 2>/dev/null";
-
     FILE* fp = popen(full_command.c_str(), "r");
     if (fp == nullptr) {
         std::cerr << command << ": command not found\n";
         return;
     }
-
     std::array<char, 128> buffer;
     std::string result;
     bool command_output = false;
-
     while (fgets(buffer.data(), buffer.size(), fp) != nullptr) {
         result += buffer.data();
         command_output = true;
     }
-
     int exit_status = pclose(fp);
     if (exit_status != 0 && !command_output) {
         std::cerr << command << ": command not found\n";
@@ -98,56 +86,40 @@ void execute_command(const std::string& command, const std::string& args) {
 
 int main() {
     bool exit = false;
-
     while (!exit) {
         std::cout << "$ ";
         std::string input;
         std::getline(std::cin, input);
-
         if (input.empty()) continue;
-
         size_t space_pos = input.find(' ');
         std::string command = input.substr(0, space_pos);
         std::string args = (space_pos == std::string::npos) ? "" : input.substr(space_pos + 1);
-
         switch (isValid(command)) {
             case cd: {
                 try {
                     if (args.empty() || args == "~") {
                         const char* home = std::getenv("HOME");
-                        if (home) {
-                            std::filesystem::current_path(home);
-                        } else {
-                            std::cerr << "cd: HOME not set\n";
-                        }
+                        if (home) filesystem::current_path(home);
+                        else std::cerr << "cd: HOME not set\n";
                     } else if (args[0] == '~') {
                         const char* home = std::getenv("HOME");
-                        if (home) {
-                            std::string newPath = std::string(home) + args.substr(1);
-                            std::filesystem::current_path(newPath);
-                        } else {
-                            std::cerr << "cd: HOME not set\n";
-                        }
-                    } else {
-                        std::filesystem::current_path(args);
-                    }
-                } catch (const std::filesystem::filesystem_error& e) {
+                        if (home) filesystem::current_path(std::string(home) + args.substr(1));
+                        else std::cerr << "cd: HOME not set\n";
+                    } else filesystem::current_path(args);
+                } catch (const std::filesystem::filesystem_error&) {
                     std::cerr << "cd: " << args << ": No such file or directory\n";
                 }
                 break;
             }
-
             case echo: {
-                std::string result = processQuotes(input);
-                result.erase(0, result.find(" ") + 1); 
-                std::cout << result << "\n";
+                std::string echoInput = input.substr(input.find(" ") + 1);
+                std::string processedInput = processEchoInput(echoInput);
+                std::cout << processedInput << "\n";
                 break;
             }
-
             case exit0:
                 exit = true;
                 break;
-
             case type: {
                 if (isValid(args) != invalid) {
                     std::cout << args << " is a shell builtin\n";
@@ -161,17 +133,14 @@ int main() {
                 }
                 break;
             }
-
             case pwd: {
                 std::cout << std::filesystem::current_path().string() << "\n";
                 break;
             }
-
             default:
                 execute_command(command, args);
                 break;
         }
     }
-
     return 0;
 }
