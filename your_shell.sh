6ee93404b734e1010b1234f53257dea8b68ad4f3
gourabsen21s <gourabsen.21.2001@gmail.com>
2025-02-05 14:47:09 +0530

    Update your_shell.sh

--- your_shell.sh
+++ your_shell.sh
@@ -8,8 +8,6 @@
 
 set -e # Exit early if any commands fail
 
-# Copied from .codecrafters/compile.sh
-#
 # - Edit this to change how your program compiles locally
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
@@ -18,8 +16,4 @@ set -e # Exit early if any commands fail
   cmake --build ./build
 )
 
-# Copied from .codecrafters/run.sh
-#
-# - Edit this to change how your program runs locally
-# - Edit .codecrafters/run.sh to change how your program runs remotely
 exec ./build/shell "$@"
